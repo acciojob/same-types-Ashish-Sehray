0475623814b16aaaebefa293b8a1973e4e746a2c
Ashish-Sehray <sehrayashish54@gmail.com>
2025-12-15 22:07:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function isSameType(value1, value2) {
   //your js code here
+	let newValue1=parseInt(value1);
+	let newValue2 =parseInt(value2);
+	console.log(newValue1,newValue2);
+	console.log(typeof(newValue1));
+	console.log(typeof(newValue2))
+	if(isNaN(newValue1)||isNaN(newValue2)){
+	    if(isNaN(newValue1)){
+	        if(isNaN(newValue2)){
+	        return true;}
+	    }
+	     if(isNaN(newValue2)){
+	        if(isNaN(newValue1)){
+	        return true;}
+	    }
+		return false;
+	}else if(typeof(newValue1)==typeof(newValue2)){
+		return true;
+	}
 }
 
 // do not change the code below.
